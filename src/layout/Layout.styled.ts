7410f378acdb97eaa94e7ed8248e3989d20ba7af
Daniel Grychtoł <grychtol.daniel@gmail.com>
2020-11-14 17:59:47 +0100

    Update Layout

--- src/layout/Layout.styled.ts
+++ src/layout/Layout.styled.ts
@@ -3,11 +3,11 @@ import tw from "twin.macro";
 
 const StyledLayout = styled.main.attrs({
   className:
-    "flex flex-col min-h-screen w-screen items-center bg-gray-400 relative pt-24",
+    "flex flex-col min-h-screen max-w-screen items-center bg-gray-300 relative pt-24",
 })`
   & {
     nav {
-      ${tw`fixed flex top-0 left-0 w-screen bg-white p-4 justify-between items-center flex-row`}
+      ${tw`fixed flex top-0 left-0 w-screen bg-white p-4 justify-between items-center flex-row z-50`};
     }
   }
 `;
