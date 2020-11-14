4ade4b38e1632f1aacb85b12106a37eb68e4436d
Daniel Grychtoł <grychtol.daniel@gmail.com>
2020-11-14 18:01:58 +0100

    Analysis fetch

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
+import { Container } from "../components/Container/Container.styled";
+import Subtitle from "../components/Typography/Subtitle";
+import Text from "../components/Typography/Text";
+import AnalysisDisplay from "../features/AnalysisDisplay";
 import { useAnalyticsContext } from "../hooks/useAnalyticsContext";
+import { SESchema } from "../types/analytics";
+import { renderDate } from "../utils/date";
 
 const Main: React.FC = () => {
-  const { fetchLatestAnalysis } = useAnalyticsContext();
+  const { fetchLatestAnalysis, matchAnalysis } = useAnalyticsContext();
 
   useEffect(() => {
     fetchLatestAnalysis();
   }, [fetchLatestAnalysis]);
 
+  const scheduledEvents = useMemo(
+    (): SESchema[] => matchAnalysis?.scheduledEvents ?? [],
+    [matchAnalysis]
+  );
+
+  if (!matchAnalysis) {
+    return (
+      <Container>
+        <Subtitle>Brak analiz</Subtitle>
+      </Container>
+    );
+  }
+
   return (
-    <>
-      <h1>app dashboard</h1>
-    </>
+    <Container>
+      <Subtitle>Najnowsza analiza</Subtitle>
+      <Text light>{"Data: " + renderDate(matchAnalysis?.createdAt)}</Text>
+
+      <AnalysisDisplay {...{ scheduledEvents }} />
+    </Container>
   );
 };
 
