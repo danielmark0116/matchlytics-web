36581d53c9c3024559482b17abe4e3455427d597
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-03-26 14:29:55 +0100

    Fix undefined goalsttRoundsEnd issue

--- src/features/HistoryMatchBlob.tsx
+++ src/features/HistoryMatchBlob.tsx
@@ -13,8 +13,10 @@ interface Props {
 }
 
 const HistoryMatchBlob: React.FC<Props> = ({
-  event: { title, date, matchDetailsLink, goals, goalsAtRoundsEnd },
+  event: { title, date, matchDetailsLink, goals, ...rest },
 }) => {
+  const goalsAtRoundsEnd = rest?.goalsAtRoundsEnd || []
+
   const sumGoalsForEachHalf = (
     goals: GoalSchema[]
   ): { firstHalf: number; secondHalf: number } => {
